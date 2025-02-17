3e9fc95bfae8b4170247d51755839ab23c35f1c8
Wesley Fisher <63987753+wesbyte29@users.noreply.github.com>
2025-02-17 16:39:57 -0600

    Update prFiller.py

--- prFiller.py
+++ prFiller.py
@@ -1,18 +1,41 @@
 import keyboard
-from pynput import mouse
+from pynput.mouse import Button, Controller
+import time
 
-def on_click(x, y, button, pressed):
-    if pressed:  # Detects when a click happens
-        print(f"Mouse clicked at ({x}, {y})")
-        return False  # Stops listening after one click
 
-print("Waiting for a mouse click...")
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+mouse = Controller()
 
-print("Mouse clicked! Proceeding...")
+#print('The current pointer position is {0}'.format(
+#    mouse.position))
 
-for i in range(0, 30):
-    keyboard.write("100")
-    keyboard.press_and_release("tab")
-    
+print("Press Shift to fill in zeros, Alt for uploads")
+
+
+while True:
+    # Wait for the next event.
+    event = keyboard.read_event()
+    if event.event_type == keyboard.KEY_DOWN and event.name == 'shift':
+        for i in range(0, 30):
+            keyboard.write("100")
+            keyboard.press_and_release("tab")  
+    if event.event_type == keyboard.KEY_DOWN and event.name == 'alt':
+        mouse.position = (508, 631)
+        mouse.click(Button.left, 1)   
+        #time.sleep(2)
+        #mouse.position = (94, 329)
+        #mouse.click(Button.left, 1)
+        time.sleep(1)
+        mouse.position = (778, 187)
+        mouse.click(Button.left, 2)
+        time.sleep(.5)
+        mouse.position = (957, 785)
+        mouse.click(Button.left, 1)
+        time.sleep(9)
+        mouse.position = (947, 529)
+        mouse.click(Button.left, 1)
+        time.sleep(4)
+        mouse.scroll(0, -8)
+    if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
+        break
+
+exit(0)
